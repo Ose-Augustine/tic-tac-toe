d27e45df92d22c77dd374dfd34a889440dd233a6
Ose-Augustine <osebeckley@gmail.com>
2022-11-06 22:02:26 +0100

    Changed layout to three parts
    Previous width of 96 not responsive to new grid layout plan

--- construct.js
+++ construct.js
@@ -3,19 +3,31 @@ const Player = (name) => {
     const sayName = () => console.log(` Hello ${name}`);
     return {sayName}
 }
-
+let mapper = []; 
 let clicks = 9;
 const boxes = document.querySelectorAll('div');
 
 boxes.forEach((box) => {
-    box.addEventListener('click', () => {
+    box.addEventListener('click', (e) => {
         if (clicks % 2 == 0) {
             box.textContent = 'O'; 
         }else {
             box.textContent = 'X';
         }
+        console.log([e.target.innerText, e.target.className])
         clicks -= 1;
     })
 })
+
+function winningAlgorithm () {
+    let first = ['box1','box4','box7'];
+    let second = ['box4', 'box5','box6']; 
+    let third = ['box1','box2','box3'];
+    let fourth = ['box2','box5','box8'];
+    let fifth = ['box3','box6','box9'];
+    let sixth = ['box7','box8','box9'];
+    let eight = ['box3','box5','box7'];
+    let ninth = ['box1','box5','box9']; 
+}
 
 
