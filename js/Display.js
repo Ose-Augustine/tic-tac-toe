2c6472365d96e2284accdb7ef02c7c3ee466eed7
Ose-Augustine <osebeckley@gmail.com>
2022-11-07 21:39:15 +0100

    Added win checker and function to populate the larger board

--- js/Display.js
+++ js/Display.js
@@ -45,5 +45,45 @@ class Umpire{
         //Position is parsed to int 
         board[position] = this.whosTurn(); //whosTurn is called to alternate marker. 
         console.log(board); 
-    }        
+    }    
+}
+
+function displayWizard(mark,position){
+    let nodeClass = toString(position);//position is the textcontent of the clicked control panel node
+    let node = document.querySelector( `.${nodeClass}` );
+    node.textContent = mark ; //mark is either'X' or 'O'
+}
+
+function checkForWin(board){
+    switch(true){
+        //horizontal win 
+        case board[0]==board[1] && board[1]==board[2]:
+            alert(`The winner is: ${board[0]}`);
+            break;
+        case board[3]==board[4] && board[4]==board[5]:
+            alert(`The winner is: ${board[3]}`);
+            break;
+        case board[6]==board[7] && board[7]==board[8]:
+            alert(`The winner is: ${board[6]}`);
+            break;
+        //Vertical win 
+        case board[0]==board[3] && board[3]==board[6]:
+            alert(`The winner is: ${board[0]}`);
+            break;
+        case board[1]==board[4] && board[4]==board[7]:
+            alert(`The winner is: ${board[1]}`);
+            break;
+        case board[2]==board[5] && board[5]==board[8]:
+            alert(`The winner is: ${board[2]}`);
+            break;
+        //diagonal win
+        case board[0]==board[4] && board[4]==board[8]:
+            alert(`The winner is: ${board[0]}`);
+            break;
+        case board[2]==board[4] && board[4]==board[6]:
+            alert(`The winner is: ${board[2]}`);
+            break;
+        default:
+            alert(`No winner found`);
+    }
 }
