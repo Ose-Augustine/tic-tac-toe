b00690ce8b4d8862b03ec1209de7e1b85157f55d
Ose-Augustine <osebeckley@gmail.com>
2022-11-07 08:51:53 +0100

    Central layout for gameboard and control panel when game starts

--- construct.js
+++ construct.js
@@ -1,7 +1,7 @@
-const Player = (name) => {
-    let points = 0;
-    const sayName = () => console.log(` Hello ${name}`);
-    return {sayName}
+const Player = (name,marker) => {
+    const sayName = () => name;
+    const sayMarker = () => marker; 
+    return {sayMarker, sayName} ;
 }
 let mapper = []; 
 let clicks = 9;
@@ -19,15 +19,5 @@ boxes.forEach((box) => {
     })
 })
 
-function winningAlgorithm () {
-    let first = ['box1','box4','box7'];
-    let second = ['box4', 'box5','box6']; 
-    let third = ['box1','box2','box3'];
-    let fourth = ['box2','box5','box8'];
-    let fifth = ['box3','box6','box9'];
-    let sixth = ['box7','box8','box9'];
-    let eight = ['box3','box5','box7'];
-    let ninth = ['box1','box5','box9']; 
-}
 
 
