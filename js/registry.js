0fc3f75a1deb1abe89607e6d50f224b68efb992a
Ose-Augustine <osebeckley@gmail.com>
2022-11-07 14:21:26 +0100

    Execution would exit current file if clicks===0

--- js/registry.js
+++ js/registry.js
@@ -4,6 +4,7 @@ const Player = (name,marker) => {
     const sayMarker = () => marker;
     return {sayName, sayMarker};
 }
+let clicks = 2; 
 //Access the DOM
 let player1 = document.querySelector('.register-player-1');
 let player2 = document.querySelector('.register-player-2');
@@ -13,13 +14,19 @@ let player2 = document.querySelector('.register-player-2');
 player1.addEventListener('click',(e)=>{
     let uniqueGameId = prompt('Give this game a unique name:');
     let name = prompt('Player1 enter name:');
+    clicks = clicks - 1; 
     let info = Player(name,'X');
     e.target.style.display = 'none'; //Once player is registered, hide button
 })
 player2.addEventListener('click',(e)=>{
     let name = prompt('Player2 enter name:');
+    clicks = clicks - 1; 
     let info = Player(name,'O');
     e.target.style.display = 'none'; //Once player is registered, hide button
 });
+
+if (clicks === 0){
+    //run the next code block 
+}
 
 
