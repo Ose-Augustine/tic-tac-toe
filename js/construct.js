e4412fa1a8f53ee63d511abba92a29ea82d3718d
Ose-Augustine <osebeckley@gmail.com>
2022-11-07 14:08:00 +0100

    No change

---
+++ js/construct.js
@@ -0,0 +1,23 @@
+function Player(name, marker) {
+    const sayName = () => name;
+    const sayMarker = () => marker;
+    return { sayMarker, sayName };
+}
+let mapper = []; 
+let clicks = 9;
+const boxes = document.querySelectorAll('div');
+
+boxes.forEach((box) => {
+    box.addEventListener('click', (e) => {
+        if (clicks % 2 == 0) {
+            box.textContent = 'O'; 
+        }else {
+            box.textContent = 'X';
+        }
+        console.log([e.target.innerText, e.target.className])
+        clicks -= 1;
+    })
+})
+
+
+
