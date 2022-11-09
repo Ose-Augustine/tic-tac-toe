5d03522a8bf694bd1aa1819f4213f6005fcb2e86
Ose-Augustine <osebeckley@gmail.com>
2022-11-09 07:23:48 +0100

    Extended tools umpire has to work with

--- js/Display.js
+++ js/Display.js
@@ -10,16 +10,12 @@ const eighth = document.querySelector('.eighth');
 const ninth = document.querySelector('.ninth');
 
 let allPanels = document.querySelectorAll('.control-panel')
-//Controls the game logic 
-//Umpire accepts the markers of the two players
-//and provides the populateBoard function to accept players choice of
-//board position
-class Umpire{
-    constructor(player1, player2){
-        this.player1 = player1;
-        this.player2 = player2;
-        this.counter = 1; 
 
+//Umpire controls the game board, its population and player's turn
+//Umpire checks for win and controls the display wizard
+class Umpire{
+    constructor(){
+        this.counter = 1; 
         this.board = ['','','','','','','','',''];//Remains the same for every instance of the umpire 
     }
     //Alternate marker 
@@ -46,44 +42,47 @@ class Umpire{
         board[position] = this.whosTurn(); //whosTurn is called to alternate marker. 
         console.log(board); 
     }    
-}
-
-function displayWizard(mark,position){
-    let nodeClass = toString(position);//position is the textcontent of the clicked control panel node
-    let node = document.querySelector( `.${nodeClass}` );
-    node.textContent = mark ; //mark is either'X' or 'O'
-}
-
-function checkForWin(board){
-    switch(true){
-        //horizontal win 
-        case board[0]==board[1] && board[1]==board[2]:
-            alert(`The winner is: ${board[0]}`);
-            break;
-        case board[3]==board[4] && board[4]==board[5]:
-            alert(`The winner is: ${board[3]}`);
-            break;
-        case board[6]==board[7] && board[7]==board[8]:
-            alert(`The winner is: ${board[6]}`);
-            break;
-        //Vertical win 
-        case board[0]==board[3] && board[3]==board[6]:
-            alert(`The winner is: ${board[0]}`);
-            break;
-        case board[1]==board[4] && board[4]==board[7]:
-            alert(`The winner is: ${board[1]}`);
-            break;
-        case board[2]==board[5] && board[5]==board[8]:
-            alert(`The winner is: ${board[2]}`);
-            break;
-        //diagonal win
-        case board[0]==board[4] && board[4]==board[8]:
-            alert(`The winner is: ${board[0]}`);
-            break;
-        case board[2]==board[4] && board[4]==board[6]:
-            alert(`The winner is: ${board[2]}`);
-            break;
-        default:
-            alert(`No winner found`);
+    displayWizard(mark,position){
+        let nodeClass = toString(position);//position is the textcontent of the clicked control panel node
+        let node = document.querySelector( `.${nodeClass}` );
+        node.textContent = mark ; //mark is either'X' or 'O'
     }
+    checkForWin(board){
+        switch(true){
+            //horizontal win 
+            case board[0]==board[1] && board[1]==board[2]:
+                alert(`The winner is: ${board[0]}`);  
+                break;
+            case board[3]==board[4] && board[4]==board[5]:
+                alert(`The winner is: ${board[3]}`);
+                break;
+            case board[6]==board[7] && board[7]==board[8]:
+                alert(`The winner is: ${board[6]}`);
+                break;
+            //Vertical win 
+            case board[0]==board[3] && board[3]==board[6]:
+                alert(`The winner is: ${board[0]}`);
+                break;
+            case board[1]==board[4] && board[4]==board[7]:
+                alert(`The winner is: ${board[1]}`);
+                break;
+            case board[2]==board[5] && board[5]==board[8]:
+                alert(`The winner is: ${board[2]}`);
+                break;
+            //diagonal win
+            case board[0]==board[4] && board[4]==board[8]:
+                alert(`The winner is: ${board[0]}`);
+                break;
+            case board[2]==board[4] && board[4]==board[6]:
+                alert(`The winner is: ${board[2]}`);
+                break;
+            default:
+                alert(`No winner found`);
+        }
+    }
+    
 }
+
+ 
+
+//Controller would handle starting the game sequence, create the umpire and pass in the players name and id to the umpire collected from the registry
