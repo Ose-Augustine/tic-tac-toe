2307f67bf742cabe98e9443fbb20a1a1c87613e1
Ose-Augustine <osebeckley@gmail.com>
2022-11-09 07:06:30 +0100

    Listen functions would return an info object for use by controller

--- js/registry.js
+++ js/registry.js
@@ -11,19 +11,28 @@ let player2 = document.querySelector('.register-player-2');
 
 //Create player object for every click. 
 //Return the player's name and marker for use in local storage
-player1.addEventListener('click',(e)=>{
-    let uniqueGameId = prompt('Give this game a unique name:');
-    let name = prompt('Player1 enter name:');
-    clicks = clicks - 1; 
-    let info = Player(name,'X');
-    e.target.style.display = 'none'; //Once player is registered, hide button
-})
-player2.addEventListener('click',(e)=>{
-    let name = prompt('Player2 enter name:');
-    clicks = clicks - 1; 
-    let info = Player(name,'O');
-    e.target.style.display = 'none'; //Once player is registered, hide button
-});
+function listenForPlayer1(){
+    let info = {name:'',marker:''}; 
+    player1.addEventListener('click',(e)=>{
+        let uniqueGameId = prompt('Give this game a unique name:');
+        info.name = prompt('Player1 enter name:');
+        info.marker = 'X'
+        clicks = clicks - 1; 
+        e.target.style.display = 'none'; //Once player is registered, hide button
+    })
+    return info; 
+}
+function listenForPlayer2(){
+    let info = {name:'',marker:''}; 
+    player2.addEventListener('click',(e)=>{
+        info.name = prompt('Player2 enter name:');
+        info.marker = 'O'; 
+        clicks = clicks - 1; 
+        e.target.style.display = 'none'; //Once player is registered, hide button
+    });
+    return info; 
+}
+
 
 if (clicks === 0){
     //run the next code block 
